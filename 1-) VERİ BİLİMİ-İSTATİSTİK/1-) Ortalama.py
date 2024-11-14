ee2832e51f2db5abfca4ec367f12b3c84ca99cb0
MuratPembegul <mpembegul15@gmail.com>
2024-11-14 10:45:30 +0100

    Update 1-) Ortalama.py

--- 1-) VERİ BİLİMİ-İSTATİSTİK/1-) Ortalama.py
+++ 1-) VERİ BİLİMİ-İSTATİSTİK/1-) Ortalama.py
@@ -1,4 +1,5 @@
-""" Ortalama, bir veri setindeki tüm değerlerin toplamının, 
+""" ORTALAMA NEDİR? 
+Ortalama, bir veri setindeki tüm değerlerin toplamının, 
 o veri setindeki eleman sayısına bölünmesiyle elde edilen merkezi bir ölçüdür. 
 İstatistikte ortalama, genellikle veri setinin genel eğilimini veya merkezini göstermek için kullanılır. Aritmetik ortalama, yaygın olarak kullanılan en basit ortalama türüdür 
 ve çoğu durumda "ortalama" denildiğinde aritmetik ortalama kastedilir.
